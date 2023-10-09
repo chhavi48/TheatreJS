b5aeabf542b778af68e5654e6252abdd421effbb
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-10-10 00:05:38 +0530

    done the base swtup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import FantasyBook from './modelComponents/FantasyBook'
+import { Canvas, useFrame } from "@react-three/fiber";
+import { ScrollControls, useScroll } from "@react-three/drei";
+import { getProject, val } from "@theatre/core";
+import {
+  SheetProvider,
+  PerspectiveCamera,
+  useCurrentSheet,
+} from "@theatre/r3f";
+import FantasyBook from "./modelComponents/FantasyBook";
 
 function App() {
+  const sheet = getProject("Fly Through 9").sheet("Scene");
   return (
     <>
-      <Canvas camera={{ fov: 75, near: 1, far: 1000, position: [7, 15, 60] }}>
-        <color attach='background' args={['black']} />
-        <OrbitControls />
-        <ambientLight />
-        <FantasyBook />
+      <Canvas gl={{ preserveDrawingBuffer: true }}>
+      <ScrollControls pages={5}>
+      <SheetProvider sheet={sheet}>
+        <Scene />
+        </SheetProvider>
+      </ScrollControls>
+      
+      
       </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function Scene() {
+  const sheet = useCurrentSheet();
+  const scroll = useScroll();
+  useFrame(() => {
+    const seqleng = val(sheet.sequence.pointer.length);
+    sheet.sequence.position = scroll.offset * seqleng;
+  });
+
+  return (
+    <>
+      <color attach="background" args={["black"]} />
+
+      <ambientLight />
+      <FantasyBook />
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 0, 0]}
+        fov={90}
+        near={0.1}
+        far={70}
+      />
+    </>
+  );
+}
