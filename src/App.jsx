1416f05e2f20dcea47e70277356c50ffeb6464fa
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-10-10 23:57:05 +0530

    finalprops

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,59 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { ScrollControls, useScroll } from "@react-three/drei";
-import { getProject, val } from "@theatre/core";
-import {
-  SheetProvider,
-  PerspectiveCamera,
-  useCurrentSheet,
-} from "@theatre/r3f";
-import FantasyBook from "./modelComponents/FantasyBook";
+import { Canvas, useFrame } from '@react-three/fiber'
+import { ScrollControls, useScroll } from '@react-three/drei'
+import { getProject, val } from '@theatre/core'
+import { SheetProvider, PerspectiveCamera, useCurrentSheet } from '@theatre/r3f'
+import FantasyBook from './modelComponents/FantasyBook'
+
+import flyThroughState from './fly.json'
 
 function App() {
-  const sheet = getProject("Fly Through 2").sheet("Scene");
+  const sheet = getProject('Fly Through', { state: flyThroughState }).sheet(
+    'Scene'
+  )
   return (
     <>
       <Canvas gl={{ preserveDrawingBuffer: true }}>
-      <ScrollControls pages={5} damping={1} maxSpeed={1}>
-      <SheetProvider sheet={sheet}>
-        <Scene />
-        </SheetProvider>
-      </ScrollControls>
-      
-      
+        <ScrollControls pages={5} damping={1} maxSpeed={0.1}>
+          <SheetProvider sheet={sheet}>
+            <Scene />
+          </SheetProvider>
+        </ScrollControls>
       </Canvas>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
+//
+//
 
 function Scene() {
-  const sheet = useCurrentSheet();
-  const scroll = useScroll();
+  const sheet = useCurrentSheet()
+  const scroll = useScroll()
+
+  // our callback will run on every animation frame
   useFrame(() => {
-    const seqleng = val(sheet.sequence.pointer.length);
-    sheet.sequence.position = scroll.offset * seqleng;
-  });
+    // the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length)
+    // update the "position" of the playhead in the sequence, as a fraction of its whole length
+    sheet.sequence.position = scroll.offset * sequenceLength
+  })
 
   return (
     <>
-      <color attach="background" args={["black"]} />
+      <color attach='background' args={['black']} />
 
       <ambientLight />
       <FantasyBook />
       <PerspectiveCamera
-        theatreKey="Camera"
+        theatreKey='Camera'
         makeDefault
         position={[0, 0, 0]}
         fov={90}
-        near={0.2}
+        near={0.1}
         far={70}
       />
     </>
-  );
+  )
 }
